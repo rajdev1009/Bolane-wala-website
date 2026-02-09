306f8d6e4fadcd2e9f006ed5a86820e347bba950
rajdev1009 <mrdev4772@gmail.com>
2026-02-09 23:23:09 +0530

    Create movies_db.py

---
+++ movies_db.py
@@ -0,0 +1,12 @@
+# movies_db.py
+# Format: "movie ka naam": "telegram post ka link"
+# Telegram post link lene ke liye channel me message par Right Click kare -> 'Copy Post Link'
+
+data = {
+    "iron man": "https://t.me/c/YOUR_CHANNEL_ID/10",
+    "avengers": "https://t.me/c/YOUR_CHANNEL_ID/11",
+    "pathaan": "https://t.me/c/YOUR_CHANNEL_ID/12",
+    "pushpa": "https://t.me/c/YOUR_CHANNEL_ID/13",
+    "spiderman": "https://t.me/c/YOUR_CHANNEL_ID/14",
+    # Yahan aur add karte jayein
+}
